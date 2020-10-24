63cb7bb44d2ded66c9ef4992ca82c1e64d1445cb
Zhe Xuan Chua <bluefish6543@gmail.com>
2020-10-24 16:05:36 +0100

    Remove redundant intent_map dictionary

--- chatbot.py
+++ chatbot.py
@@ -1,13 +1,6 @@
 from flask import Flask, request
 
 app = Flask(__name__)
-
-# Map of current intent to next allowable intents
-# Basically the flowchart logic
-intent_map = {
-    # TODO
-    None: "None"
-}
 error_threshold = 0.5
 
 
@@ -27,15 +20,12 @@ def unknown_intent():
 
 
 def predict_intent(input_text, current_intent):
-    next_intents = intent_map[current_intent]
-
     # TODO: Predict scores from model, e.g.
     # scores = model.predict(input_text)
     scores = []  # remove this line once we have the model working
-    # Discard predictions below threshold and if intent is not
-    # in the list of next allowable intents
+    # Discard predictions below threshold
     results = [(index, score) for index, score in enumerate(scores)
-               if score > error_threshold and index in next_intents]
+               if score > error_threshold]
 
     if not results:
         # Didn't match any intents
