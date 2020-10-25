6aaeee0f09117ea3ff7280fe0ab9777ebaea36bf
Zhe Xuan Chua <bluefish6543@gmail.com>
2020-10-25 10:36:28 +0000

    Some working JS without CSS

--- src/App.js
+++ src/App.js
@@ -3,23 +3,76 @@ import React from 'react';
 import $ from 'jquery';
 
 function App() {
-  $.ajax({
+
+  const [messages, setMessages] = React.useState([]);
+  const [userMessage, setUserMessage] = React.useState("");
+
+  const handleInputChange = event => {
+    setUserMessage(event.target.value);
+  }
+
+  const handleSubmit = event => {
+    $.ajax({
       type: "POST",
       url: "/get_output",
       contentType: "application/json",
       data: JSON.stringify({
-          input_text: "Hello",
-          startup: true
+          input_text: userMessage,
+          startup: false
       })
-  }).done(output => {
-      console.log(output);
-  });
+    }).done(response => {
+      const updatedMessages = messages.concat(response);
+      setMessages(updatedMessages);
+    });
+
+    event.preventDefault();
+  };
 
   return (
     <div className="App">
-      <p>Test</p>
+      <header className="flex flex-row flex-center">
+        <div className="center-element block title">
+          Chatbot
+        </div>
+      </header>
+      <div className="flex flex-row flex-column-expand content">
+        <main className="flex flex-column messages">
+          <List list={messages} />
+        </main>
+        <form
+          className="flex flex-row flex-space-between"
+          onSubmit={handleSubmit}
+        >
+          <input
+            type="text"
+            className="input-send"
+            onChange={handleInputChange}
+          />
+          <button type="submit" className="button-send">Send</button>
+        </form>
+      </div>
     </div>
   );
 }
+
+const List = ({ list }) => {
+  if (list !== undefined && list.length > 0) {
+    return (
+      <div>
+        {list.map((item, index) =>
+          <Item key={index} item={item} />
+        )}
+      </div>
+    );
+  } else {
+    return (<></>);
+  }
+};
+
+const Item = ({ item }) => (
+  <div className="message">
+    <p className="text">{item}</p>
+  </div>
+);
 
 export default App;
